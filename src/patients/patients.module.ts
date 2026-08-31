f19abf57c1dec5f1a4cdce70512129d0c5bce4fb
asifontes77 <122127937+asifontes77@users.noreply.github.com>
2026-08-31 08:49:15 -0400

    fix(patients): provide authorization service to permission guard

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthorizationModule } from '../authorization/authorization.module';
 import { PatientsController } from './patients.controller';
 import { PatientsService } from './patients.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,7 +7,7 @@ import { Patient } from './patients.entity';
 import { LaboratoryModule } from 'src/laboratory/laboratory.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Patient]), LaboratoryModule],
+  imports: [AuthorizationModule, TypeOrmModule.forFeature([Patient]), LaboratoryModule],
   controllers: [PatientsController],
   providers: [PatientsService],
   exports: [PatientsService],
