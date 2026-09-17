317036317591eb372d39d96c9866a8087d29d231
asifontes77 <122127937+asifontes77@users.noreply.github.com>
2026-09-16 21:56:32 -0400

    feat(exam-catalog): support multi-group price adjustments

---
+++ src/exam_lists/dto/price-adjustment.dto.ts
@@ -0,0 +1,8 @@
+﻿export type PriceAdjustmentOperation = 'increase' | 'decrease';
+
+export class PriceAdjustmentDto {
+  examIds: number[];
+  operation: PriceAdjustmentOperation;
+  percentage: number;
+  tariffIds: number[];
+}
